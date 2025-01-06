6e87f86b8c40c8f5b0d30002e07df852c58a6e4a
chonibe <47850011+chonibe@users.noreply.github.com>
2025-01-06 16:04:51 +0200

    Update button.jsx

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,7 +1,6 @@
 'use client';
 
 import * as React from "react"
-// ... rest of the codeimport * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva } from "class-variance-authority"
 import { cn } from "@/lib/utils"
